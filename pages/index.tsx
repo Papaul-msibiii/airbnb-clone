636014b4048f3711234b3a2f6d90170f0f9d73f6
Paul Gomis <gomispapaul6@gmail.com>
2022-07-05 14:09:42 +0000

    building the main section

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,8 +12,20 @@ const Home: NextPage = () => {
 
       <Header />
       <Banner />
+
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+
+          {/* Pull some data from a server - API endpoints */}
+        </section>
+      </main>
     </div>
   )
 }
 
 export default Home
+
+export async function getStaticProps() {
+  
+}
