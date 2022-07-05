b27a3917003acb9bf6dc6ae9930f8e853b15786d
Paul Gomis <gomispapaul6@gmail.com>
2022-07-05 13:56:09 +0000

    building banner done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
+import Banner from '../components/Banner'
 const Home: NextPage = () => {
   return (
     <div className="">
@@ -10,7 +11,7 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
-      {/* Banner */}
+      <Banner />
     </div>
   )
 }
