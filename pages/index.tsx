ea217529b188001488fb28d40ba3ab97a8b3140d
Paul Gomis <gomispapaul6@gmail.com>
2022-07-05 14:31:07 +0000

    implementing server side rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
-const Home: NextPage = () => {
+const Home: NextPage = ({exploreData}) => {
   return (
     <div className="">
       <Head>
@@ -18,6 +18,10 @@ const Home: NextPage = () => {
           <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
 
           {/* Pull some data from a server - API endpoints */}
+          {exploreData?.map((item) => (
+            <h1>{item.location}</h1>
+          )
+          )}
         </section>
       </main>
     </div>
@@ -27,5 +31,14 @@ const Home: NextPage = () => {
 export default Home
 
 export async function getStaticProps() {
-  
+  const exploreData = await fetch("https://links.papareact.com/pyp").
+  then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+    },
+  };
 }
